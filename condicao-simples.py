a856a0007221c1057e0deebb3dc7b628970fbdc7
André Melchior <68424109+andremelchior@users.noreply.github.com>
2025-03-24 11:53:17 -0300

    feat: add simple conditional structure algorithm

---
+++ condicao-simples.py
@@ -0,0 +1,9 @@
+letra = input('Letra? ')
+
+if letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o' or \
+   letra == 'u' or \
+   letra == 'A' or letra == 'E' or letra == 'I' or letra == 'O' or \
+   letra == 'U':
+    print('vogal')
+else:
+    print('não vogal')
