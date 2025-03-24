2b87c8e3a53eb00197fa47ba82e559dec4462080
André Melchior <68424109+andremelchior@users.noreply.github.com>
2025-03-24 12:47:36 -0300

    feat: add algorithm to display months with fused chained conditional structure (elif)

---
+++ meses2.py
@@ -0,0 +1,29 @@
+n = 0
+while n < 13:
+    m = int(input('mês? '))
+    if m == 1:
+        print('January')
+    elif m == 2:
+        print('Frebuary')
+    elif m == 3:
+        print('March')
+    elif m == 4:
+        print('April')
+    elif m == 5:
+        print('May')
+    elif m == 6:
+        print('June')
+    elif m == 7:
+        print('July')
+    elif m == 8:
+        print('August')
+    elif m == 9:
+        print('September')
+    elif m == 10:
+        print('Octuber')
+    elif m == 11:
+        print('November')
+    else:
+        print('December')
+                            
+    n = n + 1
